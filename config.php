15dd9e890d029bbdfd3b895eccbd3dbed678b103
klebermkardel <molinakardel@gmail.com>
2023-04-06 11:25:56 -0300

    Comenta o código do arquivo config.php

--- config.php
+++ config.php
@@ -1,12 +1,10 @@
 <?php
-
+    // Define as informações de conexão com o banco de dados
     define('HOST', 'localhost');
     define('USER', 'root');
     define('PASS', '');
     define('BASE', 'cadastro');
 
-    $conn = new MySQLi(HOST,USER,PASS,BASE);
-
-
-
+    // Cria uma nova conexão com o banco de dados usando a classe MySQLi
+    $conn = new MySQLi(HOST, USER, PASS, BASE);
 ?>
