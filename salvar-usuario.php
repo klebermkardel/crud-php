0ac2a1ba027715fdff218836646f0552879b50f7
klebermkardel <molinakardel@gmail.com>
2023-04-06 10:29:00 -0300

    Cria código PHP para edição dos usuários no banco de dados

--- salvar-usuario.php
+++ salvar-usuario.php
@@ -23,7 +23,29 @@
 
         break;
         case "editar":
-            //code
+            $nome = $_POST["nome"];
+            $email = $_POST["email"];
+            $senha = md5($_POST["senha"]);
+            $data_nasc = $_POST["data_nasc"];
+
+            // Inserção dos dados no banco de dados
+            $sql = "UPDATE usuarios SET
+                        nome='{$nome}',
+                        email='{$email}',
+                        senha='{$senha}',
+                        data_nasc='{$data_nasc}'
+                        WHERE
+                            id=".$_REQUEST["id"];
+
+            $res = $conn->query($sql);
+
+            if($res == true) {
+                print "<script>alert('Usuário editado com sucesso!');</script>";
+                print "<script>location.href='?page=listar';</script>";
+            } else {
+                print "<script>alert('Não foi possível editar o usuário!');</script>";
+                print "<script>location.href='?page=listar';</script>";
+            }
         break;
         case "excluir":
             //code
