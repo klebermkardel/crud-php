3472fc722c48a07a642ed948c7d3eae0faa01f5f
klebermkardel <molinakardel@gmail.com>
2023-04-06 08:14:10 -0300

    Traz arquivos do Bootstrap e cria estrutura HTML no arquivo index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="pt-BR">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <title>Cadastro</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+
+    <script src="js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
