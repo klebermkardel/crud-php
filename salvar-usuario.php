22c2c9a591387ac38d6f528cef86faac808a7ca0
klebermkardel <molinakardel@gmail.com>
2023-04-06 09:18:20 -0300

    Cria código PHP para cadastro dos usuários no banco de dados exibindo mensagem de cadastro realizado com sucesso e caso não ocorra o cadastro exibe mensagem de que não foi possível realizar o cadastro e redireciona para página de listagem de usuários

--- salvar-usuario.php
+++ salvar-usuario.php
@@ -5,8 +5,21 @@
             // Recepção dos dados
             $nome = $_POST["nome"];
             $email = $_POST["email"];
-            $senha = $_POST["senha"];
+            $senha = md5($_POST["senha"]);
             $data_nasc = $_POST["data_nasc"];
+
+            // Inserção dos dados no banco de dados
+            $sql = "INSERT INTO usuarios (nome, email, senha, data_nasc) VALUES ('{$nome}', '{$email}', '{$senha}', '{$data_nasc}')";
+
+            $res = $conn->query($sql);
+
+            if($res == true) {
+                print "<script>alert('Usuário cadastrado com sucesso!');</script>";
+                print "<script>location.href='?page=listar';</script>";
+            } else {
+                print "<script>alert('Não foi possível cadastrar o usuário!');</script>";
+                print "<script>location.href='?page=listar';</script>";
+            }
 
         break;
         case "editar":
