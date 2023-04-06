f240ed20a3c766b56bbb6160b338a100d2290d79
klebermkardel <molinakardel@gmail.com>
2023-04-06 08:46:43 -0300

    Cria formulário de cadastro de usuários

--- novo-usuario.php
+++ novo-usuario.php
@@ -1,1 +1,22 @@
 <h1>Novo Usuário</h1>
+<form action="?page=salvar" method="POST">
+    <div class="mb-3">
+        <label for="nome">Nome: </label>
+        <input type="text" name="nome" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label for="email">E-mail: </label>
+        <input type="email" name="email" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label for="senha">Senha: </label>
+        <input type="password" name="senha" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label for="data_nasc">Data de Nascimento: </label>
+        <input type="date" name="data_nasc" class="form-control">
+    </div>
+    <div class="mb-3">
+        <button type="submit" class="btn btn-primary">Enviar</button>
+    </div>
+</form>
