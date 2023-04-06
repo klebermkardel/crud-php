7746fd56c300f697343cf9a2e1508025015f50c9
klebermkardel <molinakardel@gmail.com>
2023-04-06 08:24:27 -0300

    Começa a criar layout da página 'index.php' e cria arquivos 'listar-usuario.php' para exibição dos usuários cadastrados e 'novo-usuario.php' para cadastro de novos usuários

--- index.php
+++ index.php
@@ -11,8 +11,29 @@
     <title>Cadastro</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Cadastro</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="novo-usuario.php">Novo Usuário</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="listar-usuario.php">Listar Usuários</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+    </nav>
 
+    
 
     <script src="js/bootstrap.bundle.min.js"></script>
   </body>
